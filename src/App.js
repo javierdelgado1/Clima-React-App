b4a913152c1219e5dda27b6e4cb293006ac412f5
Javier <ingeniero@javierdelgado.com.ve>
2020-01-04 13:07:22 -0300

    agregando formulario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Components/Header';
+import Formulario from './Components/Formulario';
 
 import './index.css';
 function App() {
@@ -8,6 +9,16 @@ function App() {
       <Header 
       titulo='Clima App'>
       </Header>
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Formulario></Formulario>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Formulario.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Formulario(){
+
+    const handleChange = e =>{
+        
+    };
+    return (
+        <form>
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    onChange={handleChange}
+                    />
+                    <label htmlFor="ciudad">Ciudad</label>
+            </div>
+
+            <div className="input-field col s12">
+                <select 
+                    onChange={handleChange}
+                    name="pais"
+                    >
+                    <option value="">Selecciona un pais</option>
+                    <option value="US">Estados unidos</option>
+                    <option value="VE">Venezuela</option>
+                    <option value="CH">Chile</option>
+                    <option value="AR">Argentina</option>
+
+                </select>
+            </div>
+            <div className="input-field col s12">
+                <input 
+                    type="submit" 
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                    value="Buscar Clima"
+                    ></input>
+            </div>
+        </form>
+    )
+}
+
+export default Formulario;
