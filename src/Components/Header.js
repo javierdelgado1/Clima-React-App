b404ba892180f8a77765f77ea2ed187843ac418a
Javier <ingeniero@javierdelgado.com.ve>
2020-01-04 12:56:52 -0300

    agregando material desing y creando componente de header

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Header({titulo}) {
+    return (
+        <nav>
+            <div className="nav-wrapper light-blue darken-2">
+                <a href="#!" className="brand-logo">{titulo}</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
