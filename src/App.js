bad76e060742ee574db3a8b77b9854f6514899be
Javier <ingeniero@javierdelgado.com.ve>
2020-01-04 14:32:06 -0300

    mostrando errores de validacion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import Header from './Components/Header';
 import Formulario from './Components/Formulario';
+import Error from './Components/Error';
 
 import './index.css';
 function App() {
 
   const [ciudad, guardarCiudad] = useState('');
   const [pais, guardarPais] = useState('');
+  const [error, guardarError] = useState(false);
 
 
   const datosConsulta = datos => {
@@ -15,14 +17,28 @@ function App() {
     //validar que ambos campos este
 
     if(datos.ciudad === '' || datos.pais === ''){
+      guardarError(true);
       return;
     }
-
-    //ciudad y pais existe, agregar al state
-
-    guardarCiudad(datos.ciudad);
-    guardarPais(datos.pais);
+      //ciudad y pais existe, agregar al state
+      
+      guardarCiudad(datos.ciudad);
+      guardarPais(datos.pais);
+      guardarError(false);
   }
+
+    //cargar un componente condicionalmente
+    console.log(error);
+    let componente ;
+    if(error){
+      componente = <Error mensaje='Ambos campos son obligatorios' ></Error>
+    }
+    else{
+      componente = null
+      
+    }
+    console.log(componente);
+
   return (
     <div className="App">
       <Header 
@@ -37,6 +53,9 @@ function App() {
                 datosConsulta={datosConsulta}>
 
                 </Formulario>
+            </div>
+            <div className="col s12 m12">
+              {componente}
             </div>
           </div>
         </div>
