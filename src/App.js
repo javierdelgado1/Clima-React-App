9f4cc30991e337142e638a2849fca31d336ee9ec
Javier Delgado <ingeniero@javierdelgado.com.ve>
2021-04-22 17:58:02 -0400

    fix: changing path url api

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
         if(consultar) {
           const appId = '95801167a3cd6e58735485d838261321';
-          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
   
           const respuesta = await fetch(url);
           const resultado = await respuesta.json();
